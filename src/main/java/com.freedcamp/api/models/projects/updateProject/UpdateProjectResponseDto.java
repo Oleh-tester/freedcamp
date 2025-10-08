50145ff7fa772448bbbcd6912025869a6c1fb655
Oleh Khomik <khomik.oleg1904@gmail.com>
2025-10-08 14:51:40 +0300

    add UpdateProjectResponseDto class for project update responses

---
+++ src/main/java/com.freedcamp/api/models/projects/updateProject/UpdateProjectResponseDto.java
@@ -0,0 +1,28 @@
+package com.freedcamp.api.models.projects.updateProject;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@lombok.Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateProjectResponseDto {
+
+	@JsonProperty("msg")
+	private String msg;
+
+	@JsonProperty("http_code")
+	private int httpCode;
+
+	@JsonProperty("data")
+	private Data data;
+
+	@JsonProperty("error_id")
+	private int errorId;
+
+	@JsonProperty("fc_code")
+	private int fcCode;
+}
